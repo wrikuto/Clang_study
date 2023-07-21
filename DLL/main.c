6cf9b711cba4f5a9b9c719f9477d5966d5739afd
USER <user@USERnoMacBook-Pro.local>
2023-07-21 16:05:28 +0900

    auto update

--- DLL/main.c
+++ DLL/main.c
@@ -6,7 +6,7 @@
 /*   By: wrikuto <wrikuto@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 17:34:00 by wrikuto           #+#    #+#             */
-/*   Updated: 2023/07/20 23:16:45 by wrikuto          ###   ########.fr       */
+/*   Updated: 2023/07/20 23:45:12 by wrikuto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,9 @@ int	main(int argc, char **argv)
 	printf("\n");
 	
 
-	push(&stack_a, &stack_b);
+	pb(&stack_a, &stack_b);
+	pb(&stack_a, &stack_b);
+	pb(&stack_a, &stack_b);
 
 
 	printList(stack_a);
